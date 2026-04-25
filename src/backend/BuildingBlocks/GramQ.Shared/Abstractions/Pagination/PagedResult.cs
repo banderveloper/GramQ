cc384f15253f3af1bd9e0d9f7e17e448e4e2d504
banderveloper <kpssdeveloper@gmail.com>
2026-04-25 23:29:10 +0300

    chore(quiz): get quizzes remove odd wrapper and add col init to pagination

--- src/backend/BuildingBlocks/GramQ.Shared/Abstractions/Pagination/PagedResult.cs
+++ src/backend/BuildingBlocks/GramQ.Shared/Abstractions/Pagination/PagedResult.cs
@@ -2,7 +2,7 @@
 
 public sealed record PagedResult<T>
 {
-    public IReadOnlyList<T> Items { get; init; }
+    public IReadOnlyList<T> Items { get; init; } = [];
     public int Page { get; init; }
     public int PageSize { get; init; }
     public int TotalCount { get; init; }
--- src/backend/Modules/QuizManagement/GramQ.QuizManagement.Application/UseCases/Queries/GetQuizzes.cs
+++ src/backend/Modules/QuizManagement/GramQ.QuizManagement.Application/UseCases/Queries/GetQuizzes.cs
@@ -8,7 +8,7 @@ namespace GramQ.QuizManagement.Application.UseCases.Queries;
 
 public sealed record GetQuizzesQuery(QuizFilter Filter);
 
-public sealed record GetQuizzesQueryResult(PagedResult<GetQuizzesQueryResult.Quiz> Quizzes)
+public sealed record GetQuizzesQueryResult
 {
     public sealed record Quiz(
         Guid Id,
@@ -24,7 +24,7 @@ public sealed class GetQuizzesQueryHandler(
     IQuizRepository quizRepository,
     ICurrentUserContext currentUser)
 {
-    public async Task<Result<GetQuizzesQueryResult>> HandleAsync(
+    public async Task<Result<PagedResult<GetQuizzesQueryResult.Quiz>>> HandleAsync(
         GetQuizzesQuery query,
         CancellationToken cancellationToken)
     {
@@ -33,11 +33,7 @@ public sealed class GetQuizzesQueryHandler(
         var pagedQuizzes =
             await quizRepository.GetPagedAsync(query.Filter with { CreatedBy = authorFilter }, cancellationToken);
 
-        return MapToUseCaseResult(pagedQuizzes);
-    }
-
-    private static GetQuizzesQueryResult MapToUseCaseResult(PagedResult<Quiz> pagedQuizzes) =>
-        new GetQuizzesQueryResult(Quizzes: new PagedResult<GetQuizzesQueryResult.Quiz>
+        return new PagedResult<GetQuizzesQueryResult.Quiz>
         {
             Page = pagedQuizzes.Page,
             PageSize = pagedQuizzes.PageSize,
@@ -50,5 +46,6 @@ public sealed class GetQuizzesQueryHandler(
                 CreatedAt: quizAgg.CreatedAt,
                 UpdatedAt: quizAgg.UpdatedAt,
                 UpdatedBy: quizAgg.UpdatedBy)).ToList()
-        });
+        };
+    }
 }
